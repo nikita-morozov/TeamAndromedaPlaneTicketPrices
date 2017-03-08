9a8ee6e0e7d095a2238bb7f51d02e35da2cf2469
Nikita Morozov <nikita1020@gmail.com>
2017-03-08 00:59:48 -0800

    ITS ALIVEEEE!

--- server.R
+++ server.R
@@ -8,58 +8,66 @@ library("XML")
 
 options(shiny.maxRequestSize=980*1024^2)
 
-server <- function(input, output, session) {
 
-  
-   #Variables:
-   country = "US"
-   currency = "USD"
-   locale = "en-us"
-   originplace="US"
-   destinationplace="Anywhere"
-   outbounddate="2017-04-01"
-   #inbounddate="2017-12"
-   apikey="te892026803091243844897141219716"
+
+server <- function(input, output, session) {
    
-   ####################
-   ####### JSON #######
-   ####################
-   
-   routes.url <- paste0("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/",country,"/",currency,"/",locale,"/",originplace,"/",destinationplace,"/", outbounddate,"?apiKey=", apikey)
-   
-   response <- GET(routes.url)
-   query <- fromJSON(content(response, "text"))
-   
-   places <- flatten(query$Places)
-   #routes <- flatten(query$Routes)
-   quotes <- flatten(query$Quotes) %>% dplyr::select(2,7,8)
-   #carriers <- flatten(query$Carriers)
-   #currencies <-flatten(query$Currencies)
-   
-   places.countries.only <- filter(places, Type == "Country") %>% dplyr::select(2,4)
-   places.stations.only <- filter(places, Type == "Station") %>% dplyr::select(1,6,8) %>% 
-      left_join(quotes, by = c("PlaceId" = "OutboundLeg.DestinationId"))
-   places.min.price <- summarise(group_by(places.stations.only, PlaceId),m = min(MinPrice))
-   places.stations.only <- places.stations.only %>%  dplyr::select(1,3) %>% unique() %>% 
-      left_join(places.min.price)
-   
-   world <- map_data('world') %>% filter(region!='Antarctica')
-   
-   world <- mutate(world, code = iso.alpha(world$region, n=2)) %>%
-      left_join(places.countries.only, by = c("code" = "SkyscannerCode")) %>% 
-      unique()
-   
-   worldForHeatmap <- inner_join(world, places.stations.only, by = c("Name" = "CountryName")) %>% dplyr::select(long,lat,m,group)%>%  na.omit()
-   colnames(worldForHeatmap) <- c("x","y","m","group")
-   
-   ########################
-   #########PLOT###########
-   ########################
-   
-  
-  output$world.map <- renderPlotly({
-    breaks = c(0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 10000)
-    world.map <- ggplot(data = world) + 
+   output$world.map <- renderPlot({
+      
+      progress <- shiny::Progress$new()
+      progress$set(message = "Computing Data", value = 0)
+      
+      #Variables:
+      country = "US"
+      currency = "USD"
+      locale = "en-us"
+      originplace="US"
+      destinationplace="Anywhere"
+      outbounddate = input$depart.data
+      #inbounddate="2017-12"
+      apikey="te892026803091243844897141219716"
+      
+      progress$set(message = "Routing API", value = 1)
+      
+      routes.url <- paste0("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/",country,"/",currency,"/",locale,"/",originplace,"/",destinationplace,"/", outbounddate,"?apiKey=", apikey)
+      
+      response <- GET(routes.url)
+      query <- fromJSON(content(response, "text"))
+      
+      places <- flatten(query$Places)
+      #routes <- flatten(query$Routes)
+      quotes <- flatten(query$Quotes) %>% dplyr::select(2,7,8)
+      #carriers <- flatten(query$Carriers)
+      #currencies <-flatten(query$Currencies)
+      
+      progress$set(message = "Filtering Data", value = 3)
+      
+      places.countries.only <- filter(places, Type == "Country") %>% dplyr::select(2,4)
+      places.stations.only <- filter(places, Type == "Station") %>% dplyr::select(1,6,8) %>% 
+        left_join(quotes, by = c("PlaceId" = "OutboundLeg.DestinationId"))
+      places.min.price <- summarise(group_by(places.stations.only, PlaceId),m = min(MinPrice))
+      places.stations.only <- places.stations.only %>%  dplyr::select(1,3) %>% unique() %>% 
+        left_join(places.min.price)
+      
+      progress$set(message = "Plotting World Map", value = 4)
+      
+      world <- map_data('world') %>% filter(region!='Antarctica')
+      
+      world <- mutate(world, code = iso.alpha(world$region, n=2)) %>%
+        left_join(places.countries.only, by = c("code" = "SkyscannerCode")) %>% 
+        unique()
+      
+      worldForHeatmap <- inner_join(world, places.stations.only, by = c("Name" = "CountryName")) %>% dplyr::select(long,lat,m,group)%>%  na.omit()
+      colnames(worldForHeatmap) <- c("x","y","m","group")
+      
+      breaks = c(0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 10000)
+      
+      progress$set(message = "Rendering World Map", value = 5)
+      progress$set(message = "Finalizing...", value = 6)
+      
+      on.exit(progress$close())
+      
+      ggplot(data = world) + 
        geom_polygon(aes(long,lat,group=group),fill="white",colour="black",size=0.1) +
        coord_equal() + 
        scale_x_continuous(expand=c(0,0)) + 
@@ -70,8 +78,9 @@ server <- function(input, output, session) {
        scale_fill_brewer(palette = "RdYlGn", direction = -1) +
        coord_quickmap() + 
        labs(x='Longitude', y='Latitude', title = "Prices Based on Destination")
-    #Allows the plot to be interactive with mouseover
-    ggplotly(world.map)
+      
+      #Allows the plot to be interactive with mouseover
+      #ggplotly(world.map)
   })
   
 }
--- ui.R
+++ ui.R
@@ -8,13 +8,13 @@ ui <- fluidPage(
     sidebarPanel(
       sliderInput('sample.number', label = "How many samples?",
                   min = 10, max = 50, value = 50),
-      dateInput("depart.data", label = "Departure Date", value = NULL, min = NULL, startview = "month", weekstart = 0),
+      dateInput("depart.data", label = "Departure Date", value = "2017-03-20",max = "2018-03-08", min = "2017-03-15", startview = "month", weekstart = 0),
       checkboxInput('add_lines', "Add Line of Best Fit")
     ),
     
     mainPanel(
       tabsetPanel(type = "tabs", 
-        tabPanel("Minimum Price Per Destination", plotlyOutput("world.map"), textOutput("timeline.comment")),
+        tabPanel("Minimum Price Per Destination", plotOutput("world.map",width = "100%",height = "400px"), textOutput("timeline.comment")),
         tabPanel("Best Time to Buy", plotlyOutput("best.date.to.buy"), textOutput("summary.petal")),
         tabPanel("Should I buy now?", textOutput("buy.now"), tableOutput('table'))
       )  
